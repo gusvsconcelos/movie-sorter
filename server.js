7ad7b1088985319a8b59ad69933e060218a495c3
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-06-02 01:02:08 -0300

    fix: folder structure

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
